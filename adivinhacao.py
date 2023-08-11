940e3d2c601e4a87b45a085eb6986c33c4fa5b02
Rodrigo Santos Magalhaes <6210084@spdm.org.br>
2023-08-11 16:30:56 -0300

    Iniciando a codificação do programa de adivinhacao

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,2 +1,16 @@
-adivinhacao = 'teste2'
-print(adivinhacao)
+print('********************************')
+print('Bem vindo ao jogo de adivinhação')
+print('********************************')
+
+
+numero_secreto = 42
+
+chute = input('Digite o seu número: ')
+
+print('Você digitou o número: ')
+print(chute)
+
+if (numero_secreto == chute):
+    print('Você acertou')
+else:
+    print('Você errou!')
